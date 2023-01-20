4a9bb085d844ca6c398a7f82dd87a2355faf676b
Hashlama95 <nikita.9a@mail.ru>
2023-01-20 20:01:32 +0300

    Initial commit

---
+++ src/main/kotlin/Main.kt
@@ -0,0 +1,47 @@
+import java.util.logging.Filter
+
+class Some {
+    companion object {
+        var count = 0
+    }
+
+    init {
+        count++
+        println("Создано объектов: $count")
+    }
+}
+
+fun filterList(list: List<String>, filter: (String)-> Boolean) {
+    list.forEach {el ->
+        if(filter(el))
+            println(el)
+    }
+}
+
+val filter: (String) -> Boolean = {
+    it.startsWith("J")
+}
+
+fun main(args: Array<String>) {
+    val list = listOf("Java", "PHP", "Pearl", "JavaScript", "C++")
+    filterList(list, filter)
+}
+//    val db = Db.MongoDb(5,"mongo")
+//
+//    val db_2 = Db.PostgreSQL(5, "mongo", true)
+//
+//    val db_copy = db.copy(conn = "Done")
+//    if(db==db_copy)
+//        println("Они равны")
+//    else
+//        println("Они не равны")
+//    if(db_copy is Db.MongoDb) {
+//        db_copy.printInfo()
+//    }
+////   val test = Some()
+////    val test 2 = Some()
+////    val test 3 = Some()
+////    val test 4 = Some()
+//
+//    //Some.count = 5
+//}
